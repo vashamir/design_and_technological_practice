3010169f1a54acfa6986891a60df24f6c3fd6c8e
vashamir <46132987+vashamir@users.noreply.github.com>
2023-09-23 17:09:44 +0300

    1_files

---
+++ availabilityunit.pas
@@ -0,0 +1,67 @@
+unit AvailabilityUnit;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, AddUnit;
+
+type
+
+  { TAvailabilityForm }
+
+  TAvailabilityForm = class(TForm)
+    BackBut: TButton;
+    FindButton: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    procedure BackButClick(Sender: TObject);
+    procedure FindButtonClick(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  AvailabilityForm: TAvailabilityForm;
+
+
+implementation
+
+uses StartUnit;
+
+{$R *.lfm}
+
+{ TAvailabilityForm }
+
+procedure TAvailabilityForm.FindButtonClick(Sender: TObject);
+   var curProduct: TProduct;
+     dell: integer;
+begin
+   product.name :=  Edit1.Text;
+   product.country := Edit2.Text;
+   dell := 0;
+   reset(f);
+   while not (EOF(f)) do
+   begin
+        read(f, curProduct);
+        if (curProduct.name = product.name) and (curProduct.country = product.country) then dell := 1;
+   end;
+   if dell = 1 then MessageDlg('Yes, are available', mtConfirmation, mbYesNoCancel,0);
+   if dell = 0 then MessageDlg('No, are not available', mtConfirmation, mbYesNoCancel,0);
+end;
+
+procedure TAvailabilityForm.BackButClick(Sender: TObject);
+begin
+     AvailabilityForm.Hide();
+      StartForm.Show();
+end;
+
+
+end.
+
